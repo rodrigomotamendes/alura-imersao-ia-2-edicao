1207b16d41c48f57de56a02d54246557e62322d1
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2024-05-11 15:15:31 -0300

    feat: exportando o contexto para toda a aplicacao

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+
+import { DataContextProvider } from './context/dataContext';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -24,9 +25,11 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }}  />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <DataContextProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }}  />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </DataContextProvider>
   );
 }
